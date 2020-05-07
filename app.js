fcf22f262425aab53d650f7ef2b35ae3e28531c6
Kevinclane <kevinclane21@gmail.com>
2020-05-06 20:54:33 -0600

    Finished

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-
+function roll() {
+  let rng = Math.random()
+  let elem = document.getElementById("die")
+  let result = ""
+  if (rng < 1 / 6) {
+    result = 'die-1.png'
+  } else if (rng < (1 / 6) * 2) {
+    result = 'die-2.png'
+  } else if (rng < (1 / 6) * 3) {
+    result = 'die-3.png'
+  } else if (rng < (1 / 6) * 4) {
+    result = 'die-4.png'
+  } else if (rng < (1 / 6) * 5) {
+    result = 'die-5.png'
+  } else if (rng < (1 / 6) * 6) {
+    result = 'die-6.png'
+  }
+  elem.innerHTML = `<img src="${result}" alt="error loading image">`
+}
 
 
 
@@ -18,3 +36,5 @@
 // // SOURCES
 
 // https://upload.wikimedia.org/wikipedia/commons/2/2c/Alea_1.png
+
+// https://www.zedge.net/find/wallpapers/magic%20the%20gathering
